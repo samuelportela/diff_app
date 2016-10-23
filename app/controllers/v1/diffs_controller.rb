1e068fd7e4ec55ab9c4e452cf83b2538cb8c534f
Samuel Portela <negociosnainternet@gmail.com>
2016-10-23 11:18:32 -0300

    When the provided id cannot be found in database, a new object will be created

--- app/controllers/v1/diffs_controller.rb
+++ app/controllers/v1/diffs_controller.rb
@@ -76,7 +76,7 @@ class V1::DiffsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_v1_diff
-      @v1_diff = V1::Diff.find(params[:id])
+      @v1_diff = V1::Diff.find_or_initialize_by(id: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
