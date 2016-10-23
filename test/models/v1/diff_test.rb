f08d8974e04dbd5f8e1b85a4cb65f2aa768f30ef
Samuel Portela <negociosnainternet@gmail.com>
2016-10-23 20:03:55 -0300

    Added unit tests for diff_result method

--- test/models/v1/diff_test.rb
+++ test/models/v1/diff_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class V1::DiffTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should result in a message saying left and right are equal" do
+    diff = V1::Diff.new
+    diff.left = "{\"foo\":\"bar\"}"
+    diff.right = "{\"foo\":\"bar\"}"
+    diff.save
+    assert_equal('Left and Right are equal', diff.diff_result)
+  end
+  
+  test "should result in a message saying left and right have different sizes" do
+    diff = V1::Diff.new
+    diff.left = "{\"foo\":\"bar\"}"
+    diff.right = "{\"fooooo\":\"bar\"}"
+    diff.save
+    assert_equal('Left and Right have different sizes', diff.diff_result)
+  end
+  
+  test "should provide insight in where the diffs are" do
+    diff = V1::Diff.new
+    diff.left = "{\"foo\":\"bar\"}"
+    diff.right = "{\"fee\":\"bur\"}"
+    diff.save
+    assert_equal([{offset: 3, length: 2}, {offset: 9, length: 1}], diff.diff_result)
+  end
 end
