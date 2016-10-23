5ba50421c9a0723f85dc2de7ca5fb00bfb682496
Samuel Portela <negociosnainternet@gmail.com>
2016-10-23 11:01:45 -0300

    Moved logic from controller to model

--- app/controllers/v1/diffs_controller.rb
+++ app/controllers/v1/diffs_controller.rb
@@ -61,17 +61,15 @@ class V1::DiffsController < ApplicationController
     end
   end
   
-  # curl -X POST 'http://localhost:3000/v1/diff/1/left' -H "Content-Type: application/json" --data '{"content":"eyJmb28iOiJiYXIifQ=="}'
-  # this will store {"foo":"bar"}
+  # curl -X POST --data-binary "@left.json" http://localhost:3000/v1/diff/1/left
   def left
-    @v1_diff.left = Base64.decode64(params[:content])
+    @v1_diff.left = Base64.decode64(request.body.string)
     @v1_diff.save
   end
   
-  # curl -X POST 'http://localhost:3000/v1/diff/1/right' -H "Content-Type: application/json" --data '{"content":"eyJmZWUiOiJidXIifQ=="}'
-  # this will store {"fee":"bur"}
+  # curl -X POST --data-binary "@right.json" http://localhost:3000/v1/diff/1/right
   def right
-    @v1_diff.right = Base64.decode64(params[:content])
+    @v1_diff.right = Base64.decode64(request.body.string)
     @v1_diff.save
   end
 
--- app/models/v1/diff.rb
+++ app/models/v1/diff.rb
@@ -1,2 +1,35 @@
 class V1::Diff < ApplicationRecord
+  def diff_result
+    if left.eql? right then
+      'Left and Right are equal'
+    elsif left.size != right.size then
+      'Left and Right have different sizes'
+    else
+      index = 0
+      offset = 0
+      length = 0
+      
+      diffs = Array.new
+      left.chars.zip(right.chars).each do |left_char, right_char|
+        if (left_char == right_char && length > 0) then
+          diffs.push({offset: offset, length: length})
+          length = 0
+        end
+        
+        if (left_char != right_char) then
+          if (length == 0) then
+            offset = index
+          end
+          length += 1
+        end
+        index += 1
+      end
+      
+      if (length > 0) then
+        diffs.push({offset: offset, length: length})
+      end
+      
+      diffs
+    end
+  end
 end
