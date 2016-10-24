0080f2d07336acafa858c1dc63434c128393fb2e
Samuel Portela <negociosnainternet@gmail.com>
2016-10-24 01:16:28 -0300

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    resources :diffs, :path => :diff, :defaults => { :format => :json } do
-      member do
-        post :left
-        post :right
-      end
-    end
+  namespace :v1, :defaults => { :format => :json } do
+    post 'diff/:id/left', to: 'diffs#left', as: :left_diff
+    post 'diff/:id/right', to: 'diffs#right', as: :right_diff
+    get 'diff/:id', to: 'diffs#show', as: :diff
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/v1/diffs_controller_test.rb
+++ test/controllers/v1/diffs_controller_test.rb
@@ -4,22 +4,22 @@ require 'test_helper'
 class V1::DiffsControllerTest < ActionDispatch::IntegrationTest
   test "should post left content" do
     assert_difference('V1::Diff.count') do
-      post left_v1_diff_url(V1::Diff.new({id: 1}), 'eyJmb28iOiJiYXIifQ==')
+      post v1_left_diff_url(V1::Diff.new({id: 1}), 'eyJmb28iOiJiYXIifQ==')
     end
     assert_response :success
   end
   
   test "should post right content" do
     assert_difference('V1::Diff.count') do
-      post right_v1_diff_url(V1::Diff.new({id: 1}), 'eyJmZWUiOiJidXIifQ==')
+      post v1_right_diff_url(V1::Diff.new({id: 1}), 'eyJmZWUiOiJidXIifQ==')
     end
     assert_response :success
   end
 
   test "should retrieve diff result" do
-    post left_v1_diff_url(V1::Diff.new({id: 1}), 'eyJmb28iOiJiYXIifQ==')
+    post v1_left_diff_url(V1::Diff.new({id: 1}), 'eyJmb28iOiJiYXIifQ==')
     diff = V1::Diff.find(1)
-    post right_v1_diff_url(diff, 'eyJmZWUiOiJidXIifQ==')
+    post v1_right_diff_url(diff, 'eyJmZWUiOiJidXIifQ==')
     get v1_diff_url(diff)
     assert_response :success
   end
